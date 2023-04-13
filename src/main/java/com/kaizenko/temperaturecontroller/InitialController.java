b80e0261e0e393eb048ff7a734477657a472509b
fadis <fstephan@users.noreply.github.com>
2023-04-13 07:39:39 -0400

    Added initialController

---
+++ src/main/java/com/kaizenko/temperaturecontroller/InitialController.java
@@ -0,0 +1,5 @@
+package com.kaizenko.temperaturecontroller;
+
+public class InitialController {
+
+}
--- src/test/java/com/kaizenko/temperaturecontroller/TemperatureControllerTest.java
+++ src/test/java/com/kaizenko/temperaturecontroller/TemperatureControllerTest.java
@@ -8,7 +8,7 @@ class TemperatureControllerTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		fail("Not yet implemented");		
 	}
 
 }
